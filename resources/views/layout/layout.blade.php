531f76d361cfabf48a2c29fc9a640d24910137ab
Alex <bach8616@gmail.com>
2023-09-01 12:52:53 +0300

    add favicon

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,6 +5,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> @yield('title', 'Blog') </title>
+  <link rel="apple-touch-icon" sizes="180x180" href={{ asset('emblem.jpg') }} />
+  <link rel="icon" type="image/jpg" sizes="32x32" href={{ asset('emblem.jpg') }} />
+  <link rel="icon" type="image/jpg" sizes="16x16" href={{ asset('mblem.jpg') }} />
   @vite('resources/css/app.css')
 </head>
 
